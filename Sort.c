a1ebabd7028dc9f5c87bf23e26620ebf18141485
asmit203 <asmit.ganguly@gmail.com>
2022-08-14 13:42:38 +0530

    declaring required functions for the sorting

---
+++ Sort.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void insertion_sort(int arr[], int n);
+void selection_sort(int arr[], int n);
+void bubble_sort(int arr[], int n);
+void merge_sort(int arr[], int n);
+void quick_sort(int arr[], int low, int high);
+int partition(int arr[], int low, int high);
+void swap(int *a, int *b);
+int main()
+{
+    return 0;
+}
