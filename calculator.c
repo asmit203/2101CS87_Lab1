4b57bc8f67cf2af3bbf23036d5ba8aed674bde2e
asmit203 <asmit.ganguly@gmail.com>
2022-08-14 13:25:08 +0530

    Adding all the functions of a basic calculator

--- calculator.c
+++ calculator.c
@@ -19,6 +19,38 @@ int main()
         printf("\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Modulus\n6. Power\n7. Exit\n");
         int choice;
         scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Addition\n");
+            printf("Result= (a+b)= %f\n", a + b);
+            break;
+        case 2:
+            printf("Subtraction\n");
+            printf("Result= (a-b)= %f\n", a - b);
+            break;
+        case 3:
+            printf("Multiplication\n");
+            printf("Result= (a*b)= %f\n", a * b);
+            break;
+        case 4:
+            printf("Division\n");
+            printf("Result= (a/b)= %f\n", a / b);
+            break;
+        case 5:
+            printf("Modulus\n");
+            printf("Result= (a%%b)= %f\n", fmodf(a, b));
+            break;
+        case 6:
+            printf("Power\n");
+            printf("Result= (a^b)= %f\n", pow(a, b));
+            break;
+        case 7:
+            printf("Exit\n");
+            kill = 0;
+            break;
+        }
+        printf("\n*********************************************************************************************\n");
     }
     return 0;
 }
