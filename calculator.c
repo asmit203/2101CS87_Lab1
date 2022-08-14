ef7a086f1799ff2a4459f83f02d645e00d54529c
asmit203 <asmit.ganguly@gmail.com>
2022-08-14 13:22:52 +0530

    Basic driver code

--- calculator.c
+++ calculator.c
@@ -4,5 +4,21 @@
 
 int main()
 {
+    printf("\n*********************************************************************************************\n");
+    int kill = 1;
+    while (kill)
+    {
+        // inputs:
+        float a, b;
+        printf("Enter two numbers: ");
+        scanf("%f %f", &a, &b);
+        printf("a = %f, b= %f\n",a,b);
+
+        // choosing the operations:
+        printf("What is the operation you will like to perform:");
+        printf("\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Modulus\n6. Power\n7. Exit\n");
+        int choice;
+        scanf("%d", &choice);
+    }
     return 0;
 }
